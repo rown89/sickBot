e1003fe4baac853702fabe35936ac2383cb5a094
unknown <Danilo Mongelli>
2020-03-31 00:05:19 +0200

    fix test python

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 const fs = require('fs').promises;
 const bodyParser = require("body-parser");
-import {PythonShell} from 'python-shell';
+const {spawn} = require('child_process');
 
 const app = express();
 const port = 4200;
@@ -13,51 +13,43 @@ app.post("/buildChart/region/stackedBar", (req, res) => {
   const {region1, region2} = req.body;
   if(region1 && region2){
     const maxValue = region1.tamponi >= region2.tamponi ? region1.tamponi : region2.tamponi;
-    let options = {
-      mode: 'text',
-      pythonPath: '/usr/bin/python',
-      pythonOptions: ['-u'], // get print results in real-time
-      scriptPath: '/charts',
-      args: [
-        region1.data,
-        region1.denominazione_regione,
-        region1.ricoverati_con_sintomi,
-        region1.terapia_intensiva,
-        region1.totale_ospedalizzati,
-        region1.isolamento_domiciliare,
-        region1.totale_attualmente_positivi,
-        region1.nuovi_attualmente_positivi,
-        region1.dimessi_guariti,
-        region1.deceduti,
-        region1.totale_casi,
-        region1.tamponi,
-        region2.data,
-        region2.denominazione_regione,
-        region2.ricoverati_con_sintomi,
-        region2.terapia_intensiva,
-        region2.totale_ospedalizzati,
-        region2.isolamento_domiciliare,
-        region2.totale_attualmente_positivi,
-        region2.nuovi_attualmente_positivi,
-        region2.dimessi_guariti,
-        region2.deceduti,
-        region2.totale_casi,
-        region2.tamponi,
-        maxValue
-      ]
-    };
-
-    PythonShell.run('stackedBar.py', options, function (err, results) {
-      if (err){ console.log(err); throw err;};
-      console.log('results: %j', results);
-    });
-
-    pyshell.on('message', function (message) {
-      console.log("Python on: " + message);
-    });
+    const python = spawn("python", ["./charts/stackedBar.py",
+      region1.data,
+      region1.denominazione_regione,
+      region1.ricoverati_con_sintomi,
+      region1.terapia_intensiva,
+      region1.totale_ospedalizzati,
+      region1.isolamento_domiciliare,
+      region1.totale_attualmente_positivi,
+      region1.nuovi_attualmente_positivi,
+      region1.dimessi_guariti,
+      region1.deceduti,
+      region1.totale_casi,
+      region1.tamponi,
+      region2.data,
+      region2.denominazione_regione,
+      region2.ricoverati_con_sintomi,
+      region2.terapia_intensiva,
+      region2.totale_ospedalizzati,
+      region2.isolamento_domiciliare,
+      region2.totale_attualmente_positivi,
+      region2.nuovi_attualmente_positivi,
+      region2.dimessi_guariti,
+      region2.deceduti,
+      region2.totale_casi,
+      region2.tamponi,
+      maxValue
+    ]);
     
-    pyshell.end(function (err,code,signal) {
-      if (err){ console.log(err); throw err;};
+    python.stdout.on("data", data => {
+      console.log(`express stdout\n: ${data}`);
+    });
+
+    python.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    python.on("close", code => {
       res.send({ok: "Python Script Executed Correctly"});
       setTimeout(() => {
         (async () => {
@@ -68,10 +60,9 @@ app.post("/buildChart/region/stackedBar", (req, res) => {
           }
         })();
       }, 1000);
-      console.log('The exit code was: ' + code);
-      console.log('The exit signal was: ' + signal);
+      console.log(`child process close all stdio with code ${code}`);
     });
-  } else res.send("Something went wrong with the data in stackedBar Post Data");
+  } else res.send("Something went wrong with the data in radar Post Data");
 });
 
 app.post("/buildChart/region/radar", (req, res) => {
@@ -119,7 +110,7 @@ app.post("/buildChart/region/radar", (req, res) => {
       setTimeout(() => {
         (async () => {
           try {
-            await fs.unlink('./charts/generatedImages/stackedBar.png');
+            await fs.unlink('./charts/generatedImages/radar.png');
           } catch (e) {
             console.log(e);
           }
