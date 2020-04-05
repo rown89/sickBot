ed1c6c43368b33ac6cfd3865b2b31a6fb94092a4
unknown <Danilo Mongelli>
2020-04-05 15:18:04 +0200

    figure 4

--- backend/charts/r_radar.py
+++ backend/charts/r_radar.py
@@ -63,7 +63,7 @@ angles = [n / float(N) * 2 * pi for n in range(N)]
 angles += angles[:1]
 
 # Initialise the spider plot
-fig = plt.figure(figsize=(12, 11))
+fig = plt.figure(figsize=(16, 15))
 ax = plt.subplot(111, polar=True)
 
 # If you want the first axis to be on top:
