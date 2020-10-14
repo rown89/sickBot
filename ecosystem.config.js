b9dfb7c2468a34a19558e32b214d3c4e9f29b3e7
Danilo M <44890500+rown89@users.noreply.github.com>
2020-10-14 20:54:20 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
         name: "sickBot",
         script: "./index.ts",
         watch: true,
+        watch_delay: 1000,
+        ignore_watch : ["backend/charts/images"],
+        watch_options: {
+          "followSymlinks": false
+        }
         env: {
             "NODE_ENV": "development"
         },
